449461e24c3d1ac2f1fa93a700aba6406d8831d6
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-15 22:58:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
-end
+def countdown(number)
+ 
+  while number >= 0
+   puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end  
+countdown(12)
+
+def count_down_with_sleep(num)
+  
+   while num >= 0
+    sleep 5
+     puts "#{num} SECOND(S)!"
+      num -= 1
+    end
+    puts "HAPPY NEW YEAR!"
+  end
+  count_down_with_sleep(12)
